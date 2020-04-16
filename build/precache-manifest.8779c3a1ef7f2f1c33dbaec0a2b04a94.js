c184389da81cca81bf031f026a932b0c0a2025cb
llahti <llahti@student.hive.fi>
2020-04-16 13:26:06 +0300

    Init frontend example

---
+++ build/precache-manifest.8779c3a1ef7f2f1c33dbaec0a2b04a94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1a8e59829491982d697559c9f88bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c880187f2a107dd5463b",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "33cd71eadaf31e77b3c2",
+    "url": "/static/js/2.36cdb3a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36cdb3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c880187f2a107dd5463b",
+    "url": "/static/js/main.d2a93c79.chunk.js"
+  },
+  {
+    "revision": "2cc49f242123af8f6765",
+    "url": "/static/js/runtime-main.aae4cf15.js"
+  }
+]);
